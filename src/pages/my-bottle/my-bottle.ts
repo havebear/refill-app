15a66c836430566cf2cbb2a13167abcb1c403946
havebear <616749285@qq.com>
2018-01-04 21:10:20 +0800

    bgg,修改了基本所有页面的样式，弄了我的瓶子页面

--- src/pages/my-bottle/my-bottle.ts
+++ src/pages/my-bottle/my-bottle.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyBottlePage {
 
+  pet: string = "puppies";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
