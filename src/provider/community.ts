4718d6f9d17601a68343ea9577156ce8a44ef4f1
BlackTrace <1078242064@qq.com>
2018-01-04 21:27:58 +0800

    添加几个方法

--- src/provider/community.ts
+++ src/provider/community.ts
@@ -3,8 +3,7 @@ declare var JMessage: any
 export class Community
 {
 
-    register(user)
-    {
+    register(user){
        return new Promise((resolve,reject)=>{
            JMessage.register(user,
         ()=>{
@@ -17,8 +16,7 @@ export class Community
        });
     }
 
-    login(user)
-    {
+    login(user){
         return new Promise((resolv,reject)=>{
             JMessage.login(
                 user,
@@ -35,6 +33,60 @@ export class Community
         });
     }
 
+    logout(){
+        return new Promise((resolv)=>{
+            JMessage.logout();
+            resolv();
+        });
+    }
+
+    getMyinfo()
+    {
+        return new Promise((resolv,reject)=>{
+            JMessage.getMyInfo(
+                (myinfo)=>{
+                    resolv(myinfo);
+                },
+                (error)=>{
+                    reject(error);
+                }
+            );
+        }
+           );
+    }
+
+    getUserInfo(userid){
+
+        return new Promise((resolv,reject)=>{
+            JMessage.getUserInfo(
+                userid
+                ,
+                (userinfo)=>{
+                    resolv(userinfo);
+    
+                },
+                (error)=>{
+                    reject(error);
+                }
+            )
+            
+        });
+
+    }
+
+    updateMyPassword(oldpass,newpass){
+
+        return new Promise((resolve,reject)=>{
+            JMessage.updateMyPassword({oldPwd:oldpass,newPwd:newpass},
+                ()=>{
+                    resolve();
+                },
+                (error)=>{
+                    reject(error);
+                }
+            )
+        });
+    }
 
 
 }
