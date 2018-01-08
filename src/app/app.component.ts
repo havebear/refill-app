083980075b5f6d87fec91d5ae01e43a0686c8839
havebear <616749285@qq.com>
2018-01-08 23:13:15 +0800

    修改了登录的逻辑

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,27 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+
+import { UserInfoService, UserInfo } from '../provider/userinfo.service'
+import LocalStorageService from '../provider/local-storage.service'
+
 declare var JMessage: any
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = TabsPage;
+  userinfo: UserInfo;
+  userGetMeInfo: string = "/user";
+  rootPage: any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private storage: LocalStorageService, private userInfoService: UserInfoService) {
+    if (this.storage.get('token')) {
+      this.userInfoService.getMyinfo().then((user: UserInfo) => {
+        this.userinfo = user;
+        this.rootPage = TabsPage;
+      })
+    }
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
